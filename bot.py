3c609a2c3b3c51fffd3015233d7f6167e0a96102
Pierre Grimaud <grimaud.pierre@gmail.com>
2019-09-10 18:58:38 +0200

    Add tests

--- bot.py
+++ bot.py
@@ -1,9 +1,47 @@
+from dotenv import load_dotenv
+import os
+import subprocess
 from spellchecker import SpellChecker
+from pprint import pprint
 
-spell = SpellChecker()
 
-misspelled = spell.unknown(['analyze', 'anazlye'])
+# Fetch env variables
+# load_dotenv()
+# print(os.getenv('GITHUB_TOKEN'))
 
-for word in misspelled:
-    print(spell.correction(word))
-    print(spell.candidates(word))
+def clonerepo(url):
+    subprocess.run(["git", "clone", url, "repository"])
+
+
+def cleanrepo():
+    subprocess.run(["rm", "-rf", "repository"])
+
+
+def scanrepo(path):
+    for i in os.scandir(path):
+        if i.is_file() and (i.path.endswith('.md') or i.path.endswith('.rst')):
+            readfile(i.path)
+        elif i.is_dir():
+            scanrepo(i.path)
+
+
+def readfile(path):
+    file = open(path)
+    text = file.read()
+    pprint(text)
+
+
+def checkwords(words):
+    spell = SpellChecker()
+
+    misspelled = spell.unknown(words)
+
+    for word in misspelled:
+        print(spell.correction(word))
+        print(spell.candidates(word))
+
+
+scanrepo("repository")
+# scleanrepo()
+# clonerepo('https://github.com/laravel/laravel')
+# checkwords(['analyze', 'anazlye'])
